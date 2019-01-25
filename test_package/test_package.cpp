d2cfe52b7c8af4ac6c2f3612a8515a57554afee1
Eric Hargitt <eric.hargitt@gmail.com>
2019-01-25 09:31:46 -0500

    Simplify test file.

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,16 +1,7 @@
 #include <toml/toml.h>
-#include <fstream>
-#include <iostream>
 
 int main()
 {
-  std::ifstream ifs("foo.toml");
-  toml::ParseResult pr = toml::parse(ifs);
-
-  if (!pr.valid()) {
-    std::cout << pr.errorReason << std::endl;
-    return 1;
-  }
-
+  toml::ParseResult pr;
   return 0;
 }
